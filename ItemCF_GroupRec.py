337f8a54c6a654f1a70110782c025868756097f4
Wang Jinkun <550596645@qq.com>
2016-06-09 13:47:15 +0800

    Update ItemCF_GroupRec.py

--- ItemCF_GroupRec.py
+++ ItemCF_GroupRec.py
@@ -37,7 +37,7 @@ class ItemCF:
         num_items = self.num_items
         self.item_similarity = np.zeros((num_items,num_items))
 
-        for i in np.arange(0,num_items):
+        for i in range(num_items):
             r_i = train[:,i]
             self.item_similarity[i,i] = 0
             for j in np.arange(i+1,num_items):
@@ -54,26 +54,26 @@ class ItemCF:
         t1 = time.time()
         print 'Finished calculating similarity matrix in %f seconds' % (t1-t0)
 
-    def Recommendation(self,user_id,kNN):
-        # recommend a top_N recommendation list for user_id
-        train_ui = self.train_ui
+    def ItemCFPrediction(self,user_id,item_id,kNN):
+        # predict the preference of a user for an item
+        pred_score = 0
+
+        train = self.train_ui
         similarity = self.item_similarity
 
-        r_u = train_ui[user_id]
-        pred_score = r_u
-
-        rated_items = np.nonzero(r_u)
-        rated_items_idx = rated_items[0]    # items rated by user_id in train set
-        predict_items_idx = np.setdiff1d(np.arange(0,self.num_items),rated_items_idx)   # item index that has to be predicted
-        for i in predict_items_idx:
-            item_idx = i
-            neighbor_ordered = self.item_neighbor[item_idx] #
-            for neigh in neighbor_ordered[0:kNN]:
-                pred_score[i] = pred_score[i] + train_ui[user_id,neigh] * similarity[i,neigh]
+        # find the user's rating history
+        # ---Case1: user has rated item
+        if train[user_id][item_id] != 0:
+            pred_score = train[user_id][item_id]
+        else:
+            neigh_of_item = self.item_neighbor[item_id]
+            neigh = neigh_of_item[0:kNN]
+            for neigh_item in neigh:
+                pred_score = pred_score + train[user_id,neigh_item] * similarity[item_id,neigh_item]
         return pred_score
 
-    def RecommendationForGroup(self,group_id,kNN,top_N):
-        # recommend a top_N recommendation list for group_id
+    def PredictionGroupPreference(self,group_id,item_id,kNN):
+        # Predict the group group_id's preference for item_id
         train_ug = self.train_ug
         train_gi = self.train_gi
 
@@ -81,54 +81,57 @@ class ItemCF:
         users = train_ug[:,group_id]
         users_ingroup = np.nonzero(users)
         users_ingroup_idx = users_ingroup[0]
-        individual_score = np.zeros((1,self.num_items))
-        for user in users_ingroup_idx:
-            individual_score = individual_score + self.Recommendation(user,kNN)
-        group_score = individual_score / users_ingroup_idx.shape[0]
+        individual_score = 0     # predict each user's preference for item_Id and aggregate these preference score
 
-        # items that the group has selected
-        r_g = train_gi[group_id]
-        rated_items = np.nonzero(r_g)
-        rated_items_idx = rated_items[0]
-        group_score[0,rated_items_idx] = 0
-        rec_candidate = np.argsort(-group_score)
-        rec_candidate_X = rec_candidate[0]
-        rec_list = rec_candidate_X[0:top_N]
-        return rec_list
+        for user in users_ingroup_idx:
+            individual_score = individual_score + self.ItemCFPrediction(user,item_id,kNN)
+        group_score = individual_score / users_ingroup_idx.shape[0]
+        return group_score
 
     def EvaluateForGroupRec(self,kNN,top_N):
-        test_gi = self.test_gi
-        num_groups = self.num_groups
-
         precision = 0
         recall = 0
         group_count = 0
 
-        for i in np.arange(0,num_groups):
-            r_i = test_gi[i]                # firstly identify items that group i has rated
+        test_gi = self.test_gi
+        train_gi = self.train_gi
+        num_groups = self.num_groups
+
+        for group_id in range(num_groups):
+            r_i = test_gi[group_id]                        # firstly identify items that group i has rated
             test_items = np.nonzero(r_i)
             test_items_idx = test_items[0]
-            if len(test_items_idx) == 0:    # if this group did not select any items in the test set, then skip the evaluate procedure
+            if len(test_items_idx) == 0:            # if this group did not select any items in the test set, then skip the evaluate procedure
                 continue
             else:
-                rec_of_i = self.RecommendationForGroup(i,kNN,top_N)
-                hit_set = np.intersect1d(rec_of_i,test_items_idx)
+                r_i_train = train_gi[group_id]
+                train_items = np.nonzero(r_i_train)
+                train_items_idx = train_items[0]
+                pred_item_idx = np.setdiff1d(np.arange(self.num_items),train_items_idx)     # for each group, items that have not been rated in the train set should be predicted
+                pred_score = np.zeros(self.num_items)
+                # print group_id,pred_item_idx,pred_score
+                for item in pred_item_idx:
+                    pred_score[item] = self.PredictionGroupPreference(group_id,item,kNN)
+                rec_cand = np.argsort(-pred_score)      # candidate set of the recommended items for group_id
+                rec_list = rec_cand[0:top_N]
+                hit_set = np.intersect1d(rec_list,test_items_idx)
                 precision = precision + len(hit_set) / (top_N * 1.0)
                 recall = recall + len(hit_set) / (len(test_items_idx) * 1.0)
                 group_count = group_count + 1
         precision = precision / (group_count * 1.0)
         recall = recall / (group_count * 1.0)
+
         return precision,recall
 
-def test():
+def example_test():
     train_ui = load_matrix('train_ui_sample.txt',6,4)
     train_ug = load_matrix('train_ug_sample.txt',6,3)
     train_gi = load_matrix('train_gi_sample.txt',3,4)
     test_gi = load_matrix('test_gi_sample.txt',3,4)
     kNNItemCF = ItemCF(train_ui,train_ug,train_gi,test_gi)
     kNNItemCF.ItemSimilarity()
-    precision,recall= kNNItemCF.EvaluateForGroupRec(2,2)
+    precision,recall = kNNItemCF.EvaluateForGroupRec(2,2)
     print precision,recall
 
 if __name__=='__main__':
-    test()
+    example_test()
